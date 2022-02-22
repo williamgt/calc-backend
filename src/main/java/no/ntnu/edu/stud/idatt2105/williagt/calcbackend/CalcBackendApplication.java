e6c1aadf5d3c376f1ecb94fdec06e17878142515
William G. Tresselt <williagt@stud.ntnu.no>
2022-02-22 11:19:38 +0100

    initial commit

---
+++ src/main/java/no/ntnu/edu/stud/idatt2105/williagt/calcbackend/CalcBackendApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.edu.stud.idatt2105.williagt.calcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/edu/stud/idatt2105/williagt/calcbackend/CalcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.edu.stud.idatt2105.williagt.calcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
